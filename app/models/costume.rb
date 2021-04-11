7140d38ea0c10c8a4e10e7c3814817bbecd6e462
github username <github email address>
2021-04-11 19:16:51 -0400

    commit tree d2ed98667572a4810ca1336cd4323c260d7559e6

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,16 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 
-class Costume < ActiveRecord::Migration[4.2]
-
-    def change
-        create_table :costumes do |t|
-          t.string :name
-          t.integer :price
-          t.string :image_url
-          t.string :size
-          t.timestamps
-        end
-    end
-
+class Costume < ActiveRecord::Base
+#this summons my data from my files. without it, the user wouldn't be able to access the files
 end
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,7 +9,7 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
 
     def change
         create_table :costumes do |t|
