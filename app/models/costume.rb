092705c1019c613fa3d84e55c6ec831fc3a13c25
github username <github email address>
2021-04-11 17:50:36 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,16 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :costumes do |t|
+          t.string :name
+          t.integer :price
+          t.string :image_url
+          t.string :size
+          t.timestamps
+        end
+    end
+
+end
